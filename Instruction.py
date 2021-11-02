c3db7a372a1c5909d4969be169fdfd61fbb566c9
Heather <hec626@mail.usask.ca>
2021-11-02 10:35:30 -0600

    Added getters and setters to Instruction

--- Instruction.py
+++ Instruction.py
@@ -7,4 +7,22 @@ class Instruction:
     def __str__(self) -> str:
         return f"TIME: {self.time} TEMP: {self.temp}  TYPE: {self.type}"
 
+    def getTime(self):
+        return self.time
+
+    def getTemp(self):
+        return self.temp
+
+    def getType(self):
+        return self.type
+
+    def setTime(self, newTime):
+        self.time = newTime
+    
+    def setTemp(self, newTemp):
+        self.temp = newTemp
+
+    def setType(self, newType):
+        self.type = newType
+        
 
